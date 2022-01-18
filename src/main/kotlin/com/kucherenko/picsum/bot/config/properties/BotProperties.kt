415dc8f7b8cddb5a5935c74e98210ba33224c96c
a18188916 <kucherenko.a.vl@sberbank.ru>
2022-01-18 11:30:54 +0300

    remove token

--- src/main/kotlin/com/kucherenko/picsum/bot/config/properties/BotProperties.kt
+++ src/main/kotlin/com/kucherenko/picsum/bot/config/properties/BotProperties.kt
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component
 
 @Component
 data class BotProperties(
-    @Value("2118132257:AAE0gzq8VFDqgTLYYWHmalT4WdtQSugxrD8")
     val token: String,
     @Value("dekadance_colors_bot")
     val botUserName: String
